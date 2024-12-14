1be8b26679873699b22749d733d99fcc725b4ca5
UFoolItWasMeAllAlong <indyramesh@gmail.com>
2024-12-14 13:53:39 -0500

    Initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Header = (props) => {
+  return (
+    <header id="header">
+      <div className="intro">
+        <div className="overlay">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 intro-text">
+                <h1>
+                  {props.data ? props.data.title : "Loading"}
+                  <span></span>
+                </h1>
+                <p style={{ backgroundColor: '#993300' }}>
+                  {props.data ? props.data.paragraph : "Loading"}
+                </p>
+                <a
+                  href="https://aceofbladesco.com/"
+                  className="btn btn-custom btn-lg page-scroll"
+                >
+                  Book Now
+                </a>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
