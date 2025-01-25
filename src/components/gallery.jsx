3843386db0dd3ccbc45d450d5ca9e12614476f5f
UFoolItWasMeAllAlong <indyramesh@gmail.com>
2025-01-25 13:06:21 -0500

    Updated project with new dependencies and package-lock.json

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -10,104 +10,104 @@ export const Gallery = (props) => {
       </div>
       <ul className="results">
         <li className="result">
-          <a href="#">
+
             <img
               src="/img/portfolio/IMG_5206.JPEG"
               width={500}
               height={500}
               alt=""
             />
-          </a>
+
         </li>
         <li className="result">
-          <a href="#">
+
             <img
               src="/img/portfolio/IMG_6415.jpg"
               width={500}
               height={500}
               alt=""
             />
-          </a>
+
         </li>
         <li className="result">
-          <a href="#">
+
             <img
               src="img/portfolio/IMG_6416.jpg"
               width={500}
               height={500}
               alt=""
             />
-          </a>
+
         </li>
         <li className="result">
-          <a href="#">
+
             <img
               src="img/portfolio/IMG_6417.jpg"
               width={500}
               height={500}
               alt=""
             />
-          </a>
+
         </li>
         <li className="result">
-          <a href="#">
+
             <img
               src="img/portfolio/IMG_6421.jpg"
               width={500}
               height={500}
               alt=""
             />
-          </a>
+
         </li>
         <li className="result">
-          <a href="#">
+
             <img
               src="img/portfolio/IMG_6422.jpg"
               width={500}
               height={500}
               alt=""
             />
-          </a>
+
         </li>
         <li className="result">
-          <a href="#">
+
             <img
               src="img/portfolio/IMG_6423.jpg"
               width={500}
               height={500}
               alt=""
             />
-          </a>
+
         </li>
         <li className="result">
-          <a href="#">
+
             <img
               src="img/portfolio/IMG_6429.jpg"
               width={500}
               height={500}
               alt=""
             />
-          </a>
+
         </li>
         <li className="result">
-          <a href="#">
+
             <img
               src="img/portfolio/IMG_6430.jpg"
               width={500}
               height={500}
               alt=""
             />
-          </a>
+
         </li>
         <li className="result">
-          <a href="#">
+
             <img
               src="img/portfolio/IMG_6431.jpg"
               width={500}
               height={500}
               alt=""
             />
-          </a>
+
         </li>
       </ul>
 
