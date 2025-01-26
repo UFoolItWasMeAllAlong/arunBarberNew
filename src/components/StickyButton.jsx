53caf2860f142a98a4785b2132b4af88d9ecbb1b
UFoolItWasMeAllAlong <indyramesh@gmail.com>
2025-01-26 15:27:42 -0500

    Your descriptive commit message

--- src/components/StickyButton.jsx
+++ src/components/StickyButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const StickyButton = () => {
   return (
-    <div className="sticky-button-container">
+    <div className="sticky-button-container" id='stickyButton'>
       <a href="https://www.vagaro.com/aceofbladesaob">
         <button className="sticky-button">
           Book Now
