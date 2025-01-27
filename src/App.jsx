a633690924ff952e0f57fee0fcb3cc34015baa6d
UFoolItWasMeAllAlong <indyramesh@gmail.com>
2025-01-27 03:53:10 -0500

    Your descriptive commit message

--- src/App.jsx
+++ src/App.jsx
@@ -25,18 +25,21 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      {/*<Features data={landingPageData.Features} /> */}
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery} />
-      {/* <Testimonials data={landingPageData.Testimonials} /> */}
-      {/* <Team data={landingPageData.Team} />  */}
-      <Contact data={landingPageData.Contact} />
-      <StickyButton />
+    <div className="app-container">
+      <div className="app">
+        <Navigation />
+        <Header data={landingPageData.Header} />
+        {/*<Features data={landingPageData.Features} /> */}
+        <About data={landingPageData.About} />
+        <Services data={landingPageData.Services} />
+        <Gallery data={landingPageData.Gallery} />
+        {/* <Testimonials data={landingPageData.Testimonials} /> */}
+        {/* <Team data={landingPageData.Team} />  */}
+        <Contact data={landingPageData.Contact} />
+        <StickyButton />
+      </div>
     </div>
+    
   );
 };
 
